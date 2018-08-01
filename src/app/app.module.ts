e42eefaf72229e5db6a9858d0b0a6880359fa714
felipecribeiro <felipe.ribeiro@spread.com.br>
2018-08-01 20:35:10 -0300

    app dr botelho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { routes } from './app.router';
 
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { SpecialtiesComponent } from './specialties/specialties.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactUsComponent,
+    SpecialtiesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BsDropdownModule.forRoot(),
+    CarouselModule.forRoot(),
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.router.ts
@@ -0,0 +1,35 @@
+import { Routes, RouterModule } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { SpecialtiesComponent } from './specialties/specialties.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'header',
+    component: HeaderComponent
+  },
+  {
+    path: 'footer',
+    component: FooterComponent
+  },
+  {
+    path: 'about-us',
+    component: AboutUsComponent
+  },
+  {
+    path: 'contact-us',
+    component: ContactUsComponent
+  },
+  {
+    path: 'specialties',
+    component: SpecialtiesComponent
+  }
+];
