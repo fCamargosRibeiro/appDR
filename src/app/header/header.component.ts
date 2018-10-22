63adf242764752e2cff66df94728fa58dc569123
fCamargosRibeiro <flp.camargos@gmail.com>
2018-10-22 16:56:05 -0300

    correções

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,13 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit() {
   }
 
   paginaInicial(){
-    this.router.navigate(['home']);
+    this.route.navigate(['home']);
   }
 
 }
