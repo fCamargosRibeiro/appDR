228abc09402eb919dafbd0b564e4a131c9837077
fCamargosRibeiro <flp.camargos@gmail.com>
2018-10-22 15:30:47 -0300

    updates to deploy to heroku

---
+++ server.js
@@ -0,0 +1,26 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const morgan = require('morgan');
+
+const cors = require('cors');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
+
+app.use(morgan('dev'));
+app.use(cors({ origin: 'http://localhost:' + process.env.PORT }));
+
+//Routes
+app.use('/api', require('./routes/appDR.routes'));
+
+app.get('/*', function (req, res) {
+
+    res.sendFile(path.join(__dirname + '/dist/<name-of-app>/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
